265108ad49f054ab6f9520f902236588a55e84c2
cybercoderdev <dev@cybercoder.org>
2015-01-12 07:28:59 -0600

    added myfirstpy.py

---
+++ myfirstpy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Tim Layton
+# dev@cybercoder.org
+#
+#  myfirstpy.py
+#  
+# Copyright 2014 Tim Layton <dev@cybercoder.org>
+#  
+
+def main():
+	print "Hello Python..."
+	return 0
+
+if __name__ == '__main__':
+	main()
